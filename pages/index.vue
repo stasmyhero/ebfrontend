2dce472aafaa8742386098eb8e255b4ddc53a876
stasmyhero <stasmyhero@gmail.com>
2020-05-27 14:39:17 +0300

    added mainpage telegram, twitter and adv components. added animations for hover on telegram and twitter

--- pages/index.vue
+++ pages/index.vue
@@ -3,34 +3,10 @@
     <div class="mainpage-grid">
       <div class="clear-item-cont on-desktop-and-tablet" />
       <AttachedPosts :posts="attachedPosts" />
-      <div class="social-item-wrapper telegram-item-wrapper">
-        <a class="social-item telegram-item" href="#">
-          <div class="telegram-item-bg" />
-          <div class="telegram-item-logo" />
-          <div class="social-item-account" />
-          <div class="social-item-title">Наш канал<br>в&nbsp;<i>Телеграме</i></div>
-        </a>
-      </div>
-      <div class="social-item-wrapper twitter-item-wrapper">
-        <a class="social-item twitter-item" href="#">
-          <div class="twitter-item-bg" />
-          <div class="twitter-item-logo" />
-          <div class="social-item-account" />
-          <div class="social-item-title">Мы&nbsp;в<br><i>Твиттере</i></div>
-        </a>
-      </div>
-      <div class="ad-item-wrapper mainpage-ad-grid-1">
-        <div class="ad-item">
-          <a class="ad-item-link" href="#"><img class="ad-item-img" src="/images/ad-banner-350x500-1.png"></a>
-          <a class="link-to-ad-prices link-underline" href="#">Разместить рекламу</a>
-        </div>
-      </div>
-      <div class="ad-item-wrapper mainpage-ad-grid-2">
-        <div class="ad-item">
-          <a class="ad-item-link" href="#"><img class="ad-item-img" src="/images/ad-banner-350x500-2.png"></a>
-          <a class="link-to-ad-prices link-underline" href="#">Разместить рекламу</a>
-        </div>
-      </div>
+      <Telegram :link="socials.telegram" />
+      <Twitter :link="socials.twitter" />
+      <Adv pos="1" img="/images/ad-banner-350x500-1.png" link="#" />
+      <Adv pos="2" img="/images/ad-banner-350x500-2.png" link="#" />
       <Post
         v-for="mypost in posts"
         :key="mypost.id"
@@ -62,13 +38,19 @@ import postsLoader from '@/components/mixins/PostsLoader.js'
 import LoadMore from '@/components/LoadMore.vue'
 import Post from '@/components/Post.vue'
 import AttachedPosts from '@/components/AttachedPosts.vue'
+import Telegram from '@/components/Telegram.vue'
+import Twitter from '@/components/Twitter.vue'
+import Adv from '@/components/Adv.vue'
 
 export default {
   transition: 'fade',
   components: {
     LoadMore,
     Post,
-    AttachedPosts
+    AttachedPosts,
+    Telegram,
+    Twitter,
+    Adv
   },
   async asyncData ({ $axios }) {
     const res = await postsLoader.load({
@@ -95,7 +77,8 @@ export default {
     return {
       page: 2,
       isLoadedOnce: false,
-      isLoading: false
+      isLoading: false,
+      socials: urls.socials
     }
   },
   mounted () {
