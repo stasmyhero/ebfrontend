17c79baffcd410917aacc0c1984eab1ff23ce2c7
stasmyhero <stasmyhero@gmail.com>
2020-06-01 00:30:37 +0300

    added header state store

---
+++ store/header.js
@@ -0,0 +1,18 @@
+import urls from '@/assets/js/url'
+
+export const state = () => ({
+  headerClass: 'header-main-page'
+})
+
+export const mutations = {
+  setHeaderClass (state, headerClass) {
+    state.headerClass = headerClass
+  }
+}
+
+// export const actions = {
+// }
+
+export const getters = {
+  headerClass: s => s.headerClass
+}
