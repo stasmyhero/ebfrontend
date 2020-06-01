6dae7001eccc7153ef2a0aca0c29b9e9c94009ca
stasmyhero <stasmyhero@gmail.com>
2020-06-01 18:16:39 +0300

    added isMobile flag

--- components/AttachedPosts.vue
+++ components/AttachedPosts.vue
@@ -1,20 +1,29 @@
 <template>
   <div class="attached-news-cont">
-    <ul class="attached-news-list">
-      <li
-        v-for="(post, index) in posts"
-        :key="post.id"
-        class="attached-news-list-item-link-cont"
-        :class="{ 'active': currentPost.id === post.id }"
-        @mouseover.prevent="changeCurrentPost(index)"
-      >
-        <nuxt-link class="attached-news-list-item-link" :to="`/${post.category_link}/${post.slug}`">
-          <span class="news-item-header">{{ post.title }}</span>
-          <span class="news-item-subheader">{{ post.subtitle }}</span>
-        </nuxt-link>
-      </li>
-    </ul>
-    <CurrentAttachedPost :currentPost="currentPost" />
+    <template v-if="!isMobile">
+      <div class="attached-news-cont">
+        <ul class="attached-news-list">
+          <li
+            v-for="(post, index) in posts"
+            :key="post.id"
+            class="attached-news-list-item-link-cont"
+            :class="{ 'active': currentPost.id === post.id }"
+            @mouseover.prevent="changeCurrentPost(index)"
+          >
+            <nuxt-link class="attached-news-list-item-link" :to="`/${post.category_link}/${post.slug}`">
+              <span class="news-item-header">{{ post.title }}</span>
+              <span class="news-item-subheader">{{ post.subtitle }}</span>
+            </nuxt-link>
+          </li>
+        </ul>
+        <CurrentAttachedPost :current-post="currentPost" />
+      </div>
+    </template>
+    <template v-else>
+      <div v-for="post in posts" :key="post.id" class="attached-news-cont">
+        <CurrentAttachedPost :current-post="post" />
+      </div>
+    </template>
   </div>
 </template>
 
@@ -35,6 +44,11 @@ export default {
       currentPost: Object,
       attached: true,
       socialURL: urls.social
+    }
+  },
+  computed: {
+    isMobile () {
+      return this.$store.getters['header/isMobile']
     }
   },
   mounted () {
